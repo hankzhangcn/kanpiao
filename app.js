e5f6a4a2d84e896f88a440990340d31262f88085
hankzhangcn <1181282174@qq.com>
2022-05-02 22:28:04 +0800

    🗃️新增登录流水表

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ App({
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
 
+
     // 登录
     // 如果已经有token
     if(wx.getStorageSync('token'))
     {
+      var that = this;
       // 校验token是否过期
       wx.request({
         url: this.globalData.serverAddress + 'function/wx/check_token.php',
@@ -19,22 +21,52 @@ App({
           'content-type': 'application/json' //默认值
         },
         success: function(res){
+          console.log(res.data);
           // 如果没有过期
-          if(res.data == 'true')
+          if(res.data == true)
           {
+            console.log("token pass");
+
             // 直接登录展示用户信息
+          }
+          // 过期则获取并覆写
+          else
+          {
+            console.log("token expired");
+            wx.login({
+              success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                console.log("临时code: " + res.code)
+                wx.request({
+                  url: that.globalData.serverAddress + 'function/wx/login.php', //接口地址
+                  data: {code:res.code},//携带上code
+                  header: {
+                    'content-type': 'application/json' //默认值
+                  },
+                  // 前端不需要openid回调值，且为安全，不可回传给客户端。
+                  // 回调实为token，使用jsonwebtoken生成
+                  success: function (res) {
+                    wx.setStorage({
+                      key:"token",
+                      data: res.data
+                    })
+                  }
+                })
+              }
+            })
           }
         }
       })
     }
-    // 否则：没有token或者token过期了
-    // 获取code并换取新的token
+    // 否则：没有token
+    // 获取code并换取token
     else
     {
+      console.log("no token");
       wx.login({
         success: res => {
           // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          console.log("临时code: " + res.code)
+          // console.log("临时code: " + res.code)
           wx.request({
             url: this.globalData.serverAddress + 'function/wx/login.php', //接口地址
             data: {code:res.code},//携带上code
